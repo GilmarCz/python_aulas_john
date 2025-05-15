33fb1d7554338a7e588ae00ef5fc79390bb3d40e
GilmarCz <131402563+GilmarCz@users.noreply.github.com>
2025-05-15 14:45:49 -0300

    add atividade2

---
+++ atividade_2.py
@@ -0,0 +1,4 @@
+# Atividade 2
+nomeh = input("Por favor digite um nome: ")
+anoh = input("Por favor digite um ano: ")
+print(f"{nomeh} é uma valente cavaleira, nascida no ano de {anoh}. Certa manhã, {nomeh} acordou com um barulho terrivel: um dragão se aproximava da aldeia. Somente {nomeh} poderia salvar os moradores da aldeia.")
