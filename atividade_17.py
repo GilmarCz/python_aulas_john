1978f4771efc586caac0e8dff5b1746973259f0c
GilmarCz <gilmarczeika@gmail.com>
2025-05-17 15:00:06 -0300

    add atividade 17 e 18 e resolução

---
+++ atividade_17.py
@@ -0,0 +1,18 @@
+# Atividade 17
+# nome = input("Digite um nome: ")
+# if nome != "Jerry":
+#     porcao = int(input("Quantas poorções? "))
+#     preco = 5.9
+#     conta = porcao * preco
+#     print(f"Sua conta é {conta}")
+# else:
+#     print(f"Acertou misseravel!!!")    
+
+nome = input("Digite um nome: ")
+if nome == "Jerry":
+    porcao = int(input("Quantas porções? "))
+    preco = 5.9
+    conta = porcao * preco
+    print(f"Sua conta é {conta}")
+else:
+    print(f"Errou misseravel!!!") 
