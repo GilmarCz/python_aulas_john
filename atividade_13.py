65a51abe9afb1ffbc4ba77b811abff9ad146bee6
GilmarCz <gilmarczeika@gmail.com>
2025-05-17 14:51:43 -0300

    add atividade 13 e resolução

---
+++ atividade_13.py
@@ -0,0 +1,6 @@
+# Atividade 13
+largura = float(input("Largura(cm): "))
+altura = float(input("Altura(cm): "))
+area = (largura * altura)
+perimetro = (2*(largura+altura))
+print(f"Area = {area} cm2 e Perimetro = {perimetro} cm")
