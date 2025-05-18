a1f0c0cdd08e628e194984769cf49b33d96c650a
GilmarCz <gilmarczeika@gmail.com>
2025-05-18 16:12:25 -0300

    add atividade 46 e resolução

---
+++ atividade_46.py
@@ -0,0 +1,7 @@
+# Atividade 46
+n = int(input("Digite um número inteiro: "))
+print(f"Números maiores que 0 e menores que {n}:")
+i = 1
+while i < n:  # Enquanto for menor que n
+    print(i)
+    i += 1  # Incrementa em 1
