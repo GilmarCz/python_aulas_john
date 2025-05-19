942fc8bc00f1b4b66d04cf6208d8a37d4c81f854
GilmarCz <131402563+GilmarCz@users.noreply.github.com>
2025-05-19 15:28:15 -0300

    add atividade3 e resolução + resposta john

--- funcoes/atividade_3.py
+++ funcoes/atividade_3.py
@@ -2,3 +2,11 @@ def quadrado_hashtag(tamanho):
     hashtag = "#"
     return ((hashtag * tamanho) + "\n") * tamanho
 print(quadrado_hashtag(10))
+
+# numero = int(input("Quantas vezes: "))
+# def quadrado_hashtag(tamanho):
+#     contador = 0
+#     while contador < tamanho:
+#         print("#"*tamanho)
+#         contador +=1
+# quadrado_hashtag(numero)
