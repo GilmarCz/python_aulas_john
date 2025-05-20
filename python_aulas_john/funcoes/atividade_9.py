d2570775e6f1b0bc6b9ef13a7fce7f4a10bfdfb7
GilmarCz <131402563+GilmarCz@users.noreply.github.com>
2025-05-20 16:56:47 -0300

    add atividade 9 e resolução + print

--- python_aulas_john/funcoes/atividade_9.py
+++ python_aulas_john/funcoes/atividade_9.py
@@ -6,9 +6,9 @@ def o_maior_numero(x, y, z):
         maior = z
     return maior
 
-o_maior_numero(50,15,90)
+print(o_maior_numero(50,15,90))
 
 def o_maior_numero2(x, y, z):
     return max(x,y,z)
 
-o_maior_numero2(25,40,89)
+print(o_maior_numero2(25,40,89))
