a38027c87712bd9a467b9356073c234e2aaa570c
GilmarCz <131402563+GilmarCz@users.noreply.github.com>
2025-05-20 16:44:11 -0300

    add atividade 8 e resolução inversa

--- python_aulas_john/funcoes/atividade_8.py
+++ python_aulas_john/funcoes/atividade_8.py
@@ -6,3 +6,12 @@ def triangulo(x):
         contador += 1
         
 triangulo(5)
+
+def triangulo(x):
+    contador = 0
+    while contador < x:
+        elementos = "#" * (x -contador)
+        print(elementos)
+        contador += 1
+        
+triangulo(5)
