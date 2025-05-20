9da02efa11f169ce1dde81e36cff8e78814c854e
GilmarCz <131402563+GilmarCz@users.noreply.github.com>
2025-05-20 16:04:35 -0300

    usar função dentro de outra função

--- python_aulas_john/funcoes/teste.py
+++ python_aulas_john/funcoes/teste.py
@@ -14,19 +14,29 @@
 #     print(elemento)
 #     contador += 1
 
-numero = int(input("Qual o número: "))
-linha = 0
+# numero = int(input("Qual o número: "))
+# linha = 0
 
-while linha < numero:  # Quantidade de linhas que serão repetidas
-    coluna = 0
-    elemento = ""
+# while linha < numero:  # Quantidade de linhas que serão repetidas
+#     coluna = 0
+#     elemento = ""
     
-    while coluna < numero:   # Quantas vezes irá repetir dentro do primeiro while
-        if (linha + coluna) % 2 == 0:
-            elemento += "0"
-        else:
-            elemento += "1"
-        coluna += 1
+#     while coluna < numero:   # Quantas vezes irá repetir dentro do primeiro while
+#         if (linha + coluna) % 2 == 0:
+#             elemento += "0"
+#         else:
+#             elemento += "1"
+#         coluna += 1
     
-    print(elemento)
-    linha += 1
+#     print(elemento)
+#     linha += 1
+
+def cumprimentar (nome):
+    print(f"Olá, {nome}")
+    
+def cumprimentar_vezes (nome, vezes):
+    while vezes > 0:
+        cumprimentar(nome)
+        vezes -= 1
+        
+cumprimentar_vezes("Maria", 5)
