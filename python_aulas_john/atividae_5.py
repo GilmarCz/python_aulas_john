ccf5a6d5e937c3716d6157a33749fd72f04538ff
GilmarCz <131402563+GilmarCz@users.noreply.github.com>
2025-05-23 15:30:09 -0300

    add atividade 5 e resolução

---
+++ python_aulas_john/atividae_5.py
@@ -0,0 +1,17 @@
+tarefas = []
+while True:
+    funcao = input("MENU \n 1. Adicionar tarefa\n 2. Remover\n 3. Mostras Tarefas\n 4. Sair\n Opção: ")
+    if funcao == "1":
+        tarefa_add = input("Digite a tarefa: ")
+        tarefas.append(tarefa_add)    
+    elif funcao == "2":
+        print(tarefas)
+        tarefa_rem = input("Digite qual tarefa excluir: ")
+        tarefas.remove(tarefa_rem)
+    elif funcao == "3":
+        print(tarefas)
+    elif funcao == "4":
+        print(f"Saindo!!!\nSuas tarefas são: {tarefas}")
+        break
+    else:
+        print("Função Invalida!!!\nTente novamente (1 - 2 - 3 - 4)")
