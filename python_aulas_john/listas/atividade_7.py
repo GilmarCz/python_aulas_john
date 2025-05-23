bec5a9710867783e646b3a27fc78c55149536898
GilmarCz <131402563+GilmarCz@users.noreply.github.com>
2025-05-23 16:57:55 -0300

    add atividade 7 e resolução

---
+++ python_aulas_john/listas/atividade_7.py
@@ -0,0 +1,13 @@
+def lista_produtos():
+    lista =["arroz","feijão","açucar","café","macarrão"]
+    while True:
+        nome = input("Digite um produto: ")
+        if nome in lista:
+            print(f"O produto: {nome} esta na lista!")
+        elif nome == "sair":
+            break
+        else:
+           print(f"O produto: {nome} não esta na lista!") 
+           
+lista_produtos()
+    
