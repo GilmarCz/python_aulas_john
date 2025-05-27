847f55deba04cb5970e0c47140d9573798cc5941
GilmarCz <131402563+GilmarCz@users.noreply.github.com>
2025-05-27 14:56:47 -0300

    criação da pasta for, add aula_for e praticas_for, exposição e explicação do for e realização de exercicios

---
+++ python_aulas_john/for/aula_for.py
@@ -0,0 +1,16 @@
+# for
+lista = ["Maria", "Madalena", "Pedro", "Paulo", "Alex", "L"]
+for nome in lista:
+    print(nome)
+    if len(nome) > 2:
+        print(f"O {nome} tem mais que 2 caracteres")
+    else:
+        print(f"O {nome} tem menos que 2 caracteres")
+        
+# range no for
+for i in range(100): # faz a contagem do 0 a 99 (conta o indice)
+    print(i)
+for j in range(5,20): # faz a contagem do 5 a 19 (conta o indice)
+    print(j)
+for k in range(0,10,2): # faz a contagem 0 2 4 6 8 pulando de 2 em 2
+    print(k)
---
+++ python_aulas_john/for/praticas_for.py
@@ -0,0 +1,9 @@
+# digita = input("Digite algo: ")
+# for caracter in digita:
+#     #print(caracter + "*")
+#     print(caracter,end="*")
+
+num = int(input("Digite um número inteiro positivo: "))
+for i in range(-num,num+1):
+    if i != 0:        
+        print(i)
