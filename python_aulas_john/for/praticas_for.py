4ecc0edcdbb153a1c5cb6e196fbe5ea9265552a1
GilmarCz <131402563+GilmarCz@users.noreply.github.com>
2025-05-28 16:41:41 -0300

    ex. pratica de lista bidimensional/matriz

--- python_aulas_john/for/praticas_for.py
+++ python_aulas_john/for/praticas_for.py
@@ -9,21 +9,39 @@
 #         print(i)
 #         #print(i,end=" ")
 
-minha_string = "Uma obra de Carolina Maria de Jesus, que relata a vida de uma favelada"
+# minha_string = "Uma obra de Carolina Maria de Jesus, que relata a vida de uma favelada"
 
-def mais_caracteres(string1):
-    print(string1)
-    string1 = string1.replace(" ", "")  # Remove os espaços
-    mais_frequente = ""  # Inicia uma variável com uma string vazia
-    max_contagem = 0  # Variável inicializada em 0 para guardar a maior contagem
+# def mais_caracteres(string1):
+#     print(string1)
+#     string1 = string1.replace(" ", "")  # Remove os espaços
+#     mais_frequente = ""  # Inicia uma variável com uma string vazia
+#     max_contagem = 0  # Variável inicializada em 0 para guardar a maior contagem
 
-    for caractere in string1:  # Percorre a string caractere por caractere
-        contagem = string1.count(caractere)  # Conta quantas vezes o caractere aparece na string
-        if contagem > max_contagem:  # Se a contagem for maior que o valor atual de max_contagem
-            max_contagem = contagem  # Atualiza max_contagem com o novo valor
-            mais_frequente = caractere  # Armazena o caractere mais frequente até o momento
+#     for caractere in string1:  # Percorre a string caractere por caractere
+#         contagem = string1.count(caractere)  # Conta quantas vezes o caractere aparece na string
+#         if contagem > max_contagem:  # Se a contagem for maior que o valor atual de max_contagem
+#             max_contagem = contagem  # Atualiza max_contagem com o novo valor
+#             mais_frequente = caractere  # Armazena o caractere mais frequente até o momento
 
-    return f"Caractere mais frequente: '{mais_frequente}' apareceu {max_contagem} vezes"
+#     return f"Caractere mais frequente: '{mais_frequente}' apareceu {max_contagem} vezes"
 
-print(mais_caracteres(minha_string))
+# print(mais_caracteres(minha_string))
 
+#minha tentativa de resolução
+""" lista_bidimensional = [[0,1,2,3],[0,8,6,9],[1,2,7,4],[4,5,6,8]]
+num = 1
+def conta_elementos_mach(minha_matriz: list, elemento: int):
+    for i in minha_matriz.count(num):
+        return minha_matriz
+
+print(conta_elementos_mach(lista_bidimensional,num)) """
+
+lista_bidimensional = [[0,1,2,3],[0,8,6,9],[1,2,7,4],[4,5,6,8]]
+num = 1
+def conta_elementos_mach(minha_matriz: list, elemento: int):
+    contagem = 0
+    for linha in minha_matriz:
+        contagem += linha.count(elemento)  # Conta quantas vezes o elemento aparece em cada linha
+    return contagem
+
+print(conta_elementos_mach(lista_bidimensional,num))
