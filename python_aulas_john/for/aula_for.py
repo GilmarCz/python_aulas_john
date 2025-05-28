3297f358960934c9fc021e070e173b2042bd382e
GilmarCz <131402563+GilmarCz@users.noreply.github.com>
2025-05-28 15:22:01 -0300

    resolução de pratica

--- python_aulas_john/for/aula_for.py
+++ python_aulas_john/for/aula_for.py
@@ -22,14 +22,14 @@
 # for i in numero:
 #     print(i)
 
-# count() conta quantas vezes uma string ou interio tem dentro de um conjunto de dados
-minha_string = "Quantas madeiras um esquilo poderia empilhar se um esquilo pudesse empilhar madeiras"
-print(minha_string.count("a")) # Vai contar quantas vezes a letra 'a' aparece (9 vezes)
+# # count() conta quantas vezes uma string ou interio tem dentro de um conjunto de dados
+# minha_string = "Quantas madeiras um esquilo poderia empilhar se um esquilo pudesse empilhar madeiras"
+# print(minha_string.count("a")) # Vai contar quantas vezes a letra 'a' aparece (9 vezes)
 
-minha_lista = [1,2,3,5,6,6,9,9,8]
-print(minha_lista.count(6)) # Vai contar quantas vezes o 6 aparece (2 vezes)
+# minha_lista = [1,2,3,5,6,6,9,9,8]
+# print(minha_lista.count(6)) # Vai contar quantas vezes o 6 aparece (2 vezes)
 
-# Replace substitui uma string por outra string
-minha_palavra = "Oi, oi, oi amigos"
-nova_palavra = minha_palavra.replace("Oi","Olá")
-print(nova_palavra) # Olá, oi, oi amigos
+# # Replace substitui uma string por outra string
+# minha_palavra = "Oi, oi, oi amigos"
+# nova_palavra = minha_palavra.replace("Oi","Olá")
+# print(nova_palavra) # Olá, oi, oi amigos
--- python_aulas_john/for/praticas_for.py
+++ python_aulas_john/for/praticas_for.py
@@ -8,3 +8,22 @@
 #     if i != 0:        
 #         print(i)
 #         #print(i,end=" ")
+
+minha_string = "Uma obra de Carolina Maria de Jesus, que relata a vida de uma favelada"
+
+def mais_caracteres(string1):
+    print(string1)
+    string1 = string1.replace(" ", "")  # Remove os espaços
+    mais_frequente = ""  # Inicia uma variável com uma string vazia
+    max_contagem = 0  # Variável inicializada em 0 para guardar a maior contagem
+
+    for caractere in string1:  # Percorre a string caractere por caractere
+        contagem = string1.count(caractere)  # Conta quantas vezes o caractere aparece na string
+        if contagem > max_contagem:  # Se a contagem for maior que o valor atual de max_contagem
+            max_contagem = contagem  # Atualiza max_contagem com o novo valor
+            mais_frequente = caractere  # Armazena o caractere mais frequente até o momento
+
+    return f"Caractere mais frequente: '{mais_frequente}' apareceu {max_contagem} vezes"
+
+print(mais_caracteres(minha_string))
+
