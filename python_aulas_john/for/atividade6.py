c9e232ccdaee80d171b2b16f15317bb3cf82a6be
GilmarCz <131402563+GilmarCz@users.noreply.github.com>
2025-05-28 16:09:02 -0300

    atualização da resolução atividade6

--- python_aulas_john/for/atividade6.py
+++ python_aulas_john/for/atividade6.py
@@ -6,3 +6,15 @@ def sem_vogal(string1):
         nova_palavra = nova_palavra.replace(i,"")
     return nova_palavra
 print(sem_vogal(palavra))
+
+# Resolução John
+# def sem_vogal(texto):
+#     vogais ="aeiou"
+#     nova_string = ""
+    
+#     for letra in texto:
+#         if letra not in vogais:
+#             nova_string += letra
+#     return nova_string
+
+# print(sem_vogal("banana"))
