ade697111d283e3e1646ca52cce0c54a0c289949
GilmarCz <131402563+GilmarCz@users.noreply.github.com>
2025-05-30 15:29:11 -0300

    ajuste da atividade1

--- python_aulas_john/dicionario/atividade1.py
+++ python_aulas_john/dicionario/atividade1.py
@@ -1,25 +1,32 @@
-agenda = {}
-
+# Função principal que gerencia a agenda telefônica
 def agenda_telefonica(minha_agenda):
-    while True:
-        print("--- Agenda Telefonica ---")
-        print("- - Digite uma opção: - -")
-        opcao = input("Comando (1 Busca | 2 Adiciona | 3 Sair): ")
-        if opcao == "1":
-            nome = input("Nome: ")
-            if nome in agenda:
-                print(f"Número: " + agenda[nome])
+    agenda = {}  # Cria um dicionário vazio chamado 'agenda' que armazenará os contatos (nome: número)
+    while True:  # Laço infinito para manter o programa rodando até que o usuário decida sair
+        print("--- Agenda Telefonica ---")  # Título da agenda
+        print("- - Digite uma opção: - -")  # Orientação ao usuário
+        opcao = input("Comando (1 Busca | 2 Adiciona | 3 Sair): ")  # Lê a opção do usuário
+        
+        if opcao == "1":  # Se o usuário escolher buscar um número
+            nome = input("Nome: ")  # Solicita o nome a ser buscado
+            if nome in agenda:  # Verifica se o nome está na agenda
+                print(f"Número: " + agenda[nome])  # Mostra o número correspondente
             else:
-                print("Não encontrado o numero!!!")
-        elif opcao == "2":
-            nome = input("Nome: ")
-            numero = input("Número: ")
-            agenda[nome] = numero
-            print("Número adicionado!")
-        elif opcao == "3":
-            print("Saindo!")
-            break
-        else:
-            print("Comando invalido!!!\nUse ( 1 | 2 | 3 )")
+                print("Não encontrado o numero!!!")  # Mensagem de erro caso o nome não exista
+        
+        elif opcao == "2":  # Se o usuário escolher adicionar um novo contato
+            nome = input("Nome: ")  # Solicita o nome do contato
+            numero = input("Número: ")  # Solicita o número do contato
+            agenda[nome] = numero  # Adiciona o par nome:número no dicionário
+            print("Número adicionado!")  # Confirmação para o usuário
+        
+        elif opcao == "3":  # Se o usuário escolher sair do programa
+            print("Saindo!")  # Mensagem de encerramento
+            print(agenda)  # Mostra a agenda
+            break  # Encerra o laço e termina a função
+        
+        else:  # Caso o usuário digite uma opção inválida
+            print("Comando invalido!!!\nUse ( 1 | 2 | 3 )")  # Mensagem de erro
             
-agenda_telefonica(agenda)
+
+# Chama a função
+agenda_telefonica()
