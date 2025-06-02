22091c79af728abb939c9ab8f0bf11a08b1b4025
GilmarCz <131402563+GilmarCz@users.noreply.github.com>
2025-06-02 16:48:28 -0300

    add aula1 e dados.txt para explicação de conteúdo

---
+++ python_aulas_john/try_except/aula1.py
@@ -0,0 +1,20 @@
+arquivo = None
+
+try:
+    arquivo = open("dados.txt", "r")
+    conteudo = arquivo.read()
+    print("Arquivo lido com sucesso")
+    
+except FileNotFoundError:
+    print("Erro: Arquivo não encontrado")
+
+except Exception as e:
+    print("Erro ao ler o arquivo: {e}")
+    
+else:
+    print("Processamento do arquivo concluido com sucesso")
+    
+finally:
+    if arquivo:
+        arquivo.close()
+        print("Arquivo fechado")
