a63a24dfd7a246c9f15b67dbd6fa97d14ff36e20
GilmarCz <131402563+GilmarCz@users.noreply.github.com>
2025-06-02 16:13:30 -0300

    criação da pasta try_except e exposição sobre o assunto

---
+++ python_aulas_john/try_exception/aula_try_exception.py
@@ -0,0 +1,10 @@
+# Try e Except: Tratar erros
+try:
+    numero = int(input("Digite um número: "))
+    print(f"Você digitou: {numero} ")
+except ValueError:
+    print("Erro!!! Digite um número!")
+    
+# numero1 = int(input("Digite um número: "))
+# print(f"Você digitou: {numero1} ")
+
