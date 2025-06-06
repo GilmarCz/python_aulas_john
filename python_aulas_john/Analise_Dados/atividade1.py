d4277475d8c8ffb8165c657f1349d9da773f3a9b
GilmarCz <131402563+GilmarCz@users.noreply.github.com>
2025-06-06 16:43:39 -0300

    atualização da resolução

--- python_aulas_john/Analise_Dados/atividade1.py
+++ python_aulas_john/Analise_Dados/atividade1.py
@@ -5,3 +5,10 @@ def criar_array():
     array_1d = np.array(lista_numeros)
     print(array_1d)
 criar_array()
+
+# resolução John
+# Cria a função que recebe uma lista
+def criar_array1(lista):
+    return np.array(lista)
+    numeros = [1,2,3,4,5,6]
+    print(criar_array1(numeros))
