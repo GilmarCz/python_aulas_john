14c58dd0a7217dbc9a8acd1e611f0664aa111b26
GilmarCz <131402563+GilmarCz@users.noreply.github.com>
2025-06-09 15:13:37 -0300

    add atividade7 resolução do John

--- python_aulas_john/Analise_Dados/atividade7.py
+++ python_aulas_john/Analise_Dados/atividade7.py
@@ -6,6 +6,17 @@ def somar_matrizes():
         soma = array_a + array_b
         print("Soma das matrizes:", soma)
     except Exception as e:
-        print(f"Erro ao somar os arrays: {e}")
-    
+        print(f"Erro ao somar os arrays: {e}")    
 somar_matrizes()
+
+# resolução John
+def soma(matriz1, matriz2):
+    if np.shape(matriz1) == np.shape(matriz2):
+        return matriz1 + matriz2
+    else:
+        return "Erro: as matrizes devem ter o mesmo tamanho."
+    
+m1 = np.array([[1,2],[3,4]])
+m2 = np.array([[5,6],[7,8]])
+
+print(soma(m1, m2))
